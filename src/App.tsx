8c90c6e2dba3527bda49faeeeaf0215ade945ece
Eilon Lotem <eilon@tres.finance>
2022-08-09 13:09:42 +0300

    use dark colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import './App.css'
 import Button from '@mui/material/Button';
+import { alpha, styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import { Box } from '@mui/system';
 import { Stack } from '@mui/material';
@@ -135,18 +136,59 @@ const App = () => {
     }
   }
 
+  const CssTextField = styled(TextField)({
+    '& label.Mui-focused': {
+      color: 'white',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: 'white',
+      borderColor: 'white',
+      color: 'white'
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'white',
+        color: 'white'
+      },
+      '&:hover fieldset': {
+        borderColor: 'white',
+        color: 'white'
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: 'white',
+        color: 'white'
+      },
+      '&.Mui-disabled fieldset': {
+        borderColor: 'white',
+        color: 'white',
+      },
+      '&.Mui-disabled input': {
+        borderColor: 'white',
+        color: 'white',
+      },
+      '&.Mui-disabled': {
+        webkitTextFillColor: 'white'
+      },
+    },
+  });
+
   return (
     <div>
       <Box>
         <Stack spacing={2}>
-          <Button variant='contained' onClick={handleClick}>{isImpersonating ? 'Stop' : 'Start'}</Button>
-          <TextField
+          <h1>Welcome to TresConnect</h1>
+          <p>Import wallet address and start faking it.</p>
+          <CssTextField
             label="Address"
-            variant="outlined"
+            inputProps={{ style: { color: "white" } }}
+            className="text-box"
             disabled={isImpersonating}
             value={address}
             onChange={(e) => setAddress(e.target.value)}
           />
+          <Button variant='contained' onClick={handleClick}>{isImpersonating ? 'Stop' : 'Start'}</Button>
+          <p className="bottom-text">Make sure MetaMask is installed before starting.</p>
+          <p className="powered-text">Powered by <a href="https://tres.finance" target="_blank">TRES</a>.</p>
         </Stack>
       </Box>
     </div>
