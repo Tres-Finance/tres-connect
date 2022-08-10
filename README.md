2fc324d8f295fc26d2b184c45485d5fb5aa671de
Idan David <idan-david@users.noreply.github.com>
2022-08-10 17:13:31 +0300

    survive refreshes (#2)

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 # Tres Connect
 
 ## TL;DR
+
 > Readonly wallet addresses for any DeFi app
 
 ## What does extension do?
+
 Some DeFi apps do not support viewing the stats of an arbitrary address. These apps allow us to connect using a wallet provider.  
-TresConnect enables you to connect to these apps using any address you choose, in a *read only* mode (e.g. can only view, not sign messages / transactions)
+TresConnect enables you to connect to these apps using any address you choose, in a _read only_ mode (e.g. can only view, not sign messages / transactions)
 
 ## Known limitations
+
 Does not support apps that require signing a message for login (e.g. Polygon Staking)
 
 ## Requirements
+
 You must have [MetaMask](https://metamask.io/) installed, as this extension changes the results returned from MetaMask when an app asks for the current connected address.
 
 ## Developing
+
 ### Available Scripts
 
 In the project directory, you can run:
@@ -25,6 +30,7 @@ Install all required packages to build the extension.
 #### `npm run build`
 
 Builds the extension for production to the `build` folder.
+
 #### `npm run watch`
 
 Monitors `./src` and `./public` for changes, and rebuilds when changes are detected.
--- src/App.tsx
+++ src/App.tsx
@@ -1,140 +1,49 @@
 import { useCallback, useEffect, useState } from 'react';
-import './App.css'
+import './App.css';
 import Button from '@mui/material/Button';
-import { alpha, styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import { Box } from '@mui/system';
 import { Stack } from '@mui/material';
-import { setupInpageImpersonator } from './inpage';
+import {
+  getImpersonationAddress,
+  getLastUsedAddress,
+  startImpersonation,
+  stopImpersonation
+} from './common';
 
 const App = () => {
-  const [isImpersonating, setIsImpersonating] = useState(false)
-  const [address, setAddress] = useState<string>('')
-
-
-  const getActiveTabId = async (): Promise<number> => {
-    return (await chrome.tabs.query({ active: true, currentWindow: true }))[0].id || 0
-  }
-
-  const startImpersonation = async (address: string) => {
-    await chrome.scripting.executeScript({
-      func: (address) => {
-        window.impersonator?.startImpersonation(address)
-      },
-      args: [address],
-      target: {
-        tabId: await getActiveTabId()
-      },
-      world: 'MAIN',
-    });
-
-    setIsImpersonating(true)
-
-  }
-
-  const stopImpersonation = async () => {
-    await chrome.scripting.executeScript({
-      func: (address) => {
-        window.impersonator?.stopImpersonation()
-      },
-      args: [address],
-      target: {
-        tabId: await getActiveTabId()
-      },
-      world: 'MAIN',
-    });
-
-    setIsImpersonating(false)
-  }
-
-  const getImpersonationAddress = async (): Promise<string | undefined> => {
-    const [{ result: address }] = await chrome.scripting.executeScript({
-      func: () => {
-        return window.impersonator?.address
-      },
-      args: [],
-      target: {
-        tabId: await getActiveTabId()
-      },
-      world: 'MAIN',
-    });
-
-    return address;
-  }
-
-  const loadInpageScript = async () => {
-    console.debug("Loading inpage script")
-    await chrome.scripting.executeScript({
-      func: setupInpageImpersonator,
-      args: [],
-      target: {
-        tabId: await getActiveTabId()
-      },
-      world: 'MAIN',
-    });
-
-    console.debug("Inpage script loaded")
-  }
-
-  const setLastUsedAddress = (address: string) => {
-    chrome.storage.sync.set({'address': address}, () => {
-      setAddress(address)
-    });
-  }
-
-  const getLastUsedAddress = async (): Promise<string | undefined> => {
-    const { address } = await chrome.storage.sync.get('address')
-    return address
-  }
+  const [isImpersonating, setIsImpersonating] = useState(false);
+  const [address, setAddress] = useState<string>('');
 
   const loadAndSetImpersonationAddress = useCallback(async () => {
-      console.log("Getting impersonation status");
-      await loadInpageScript();
-      
-      const lastUsedAddress = await getLastUsedAddress()
-      const currentImpersonationAddress = await getImpersonationAddress()
+    console.log('Getting impersonation status');
 
-      setAddress(currentImpersonationAddress || lastUsedAddress || '')
-      setIsImpersonating(!!currentImpersonationAddress)
-  }, [])
+    const currentImpersonationAddress = await getImpersonationAddress();
+    const lastUsedAddress = await getLastUsedAddress();
+    const updatedAddress = currentImpersonationAddress || lastUsedAddress;
 
-
-  // handle isImpersonating change
-  useEffect(() => {
-    if (isImpersonating) {
-      setLastUsedAddress(address)
-    } 
-
-    chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([tab]) => {    
-      if (!tab) {
-        // when devtools of the extension are open, the tabs array is empty
-        return;
-      }    
-      chrome.action.setBadgeText({
-        text: isImpersonating ? 'ON' : '',
-        tabId: tab.id
-      }, () => {});    
-     })
-
-
-  }, [isImpersonating])
+    if (updatedAddress) {
+      setAddress(updatedAddress);
+    }
+    setIsImpersonating(!!currentImpersonationAddress);
+  }, []);
 
   // on mount
   useEffect(() => {
-    chrome.action.setBadgeBackgroundColor(
-      {color: '#ff0000'},  // red
-      () => { /* ... */ },
-    );
     loadAndSetImpersonationAddress().catch(console.error);
   }, []);
 
   const handleClick = () => {
     if (isImpersonating) {
-      stopImpersonation()
+      stopImpersonation().then(() => {
+        setIsImpersonating(false);
+      });
     } else {
-      startImpersonation(address)
+      startImpersonation(address).then(() => {
+        setIsImpersonating(true);
+      });
     }
-  }
+  };
 
   return (
     <div>
@@ -144,20 +53,27 @@ const App = () => {
           <p>Import wallet address and start faking it.</p>
           <TextField
             label="Address"
-            inputProps={{ style: { color: "white" } }}
+            inputProps={{ style: { color: 'white' } }}
             className="text-box"
             disabled={isImpersonating}
             value={address}
             onChange={(e) => setAddress(e.target.value)}
           />
-          <Button variant='contained' onClick={handleClick}>{isImpersonating ? 'Stop' : 'Start'}</Button>
+          <Button variant="contained" onClick={handleClick}>
+            {isImpersonating ? 'Stop' : 'Start'}
+          </Button>
           <p className="bottom-text">Make sure MetaMask is installed before starting.</p>
-          <p className="powered-text">Powered by <a href="https://tres.finance" target="_blank">TRES</a>.</p>
+          <p className="powered-text">
+            Powered by{' '}
+            <a href="https://tres.finance" target="_blank" rel="noreferrer">
+              TRES
+            </a>
+            .
+          </p>
         </Stack>
       </Box>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
